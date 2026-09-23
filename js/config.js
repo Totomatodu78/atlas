112dc29e22bc07ad8d00d9c6a7a90df9cf6e0d07
Totomato <thomas.cosnier@etu.univ-paris1.fr>
2026-09-23 14:17:13 +0200

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,6 @@
+
+export const SUPABASE_URL = 'https://mzkpaqqrylhzabyckimh.supabase.co';
+
+export const SUPABASE_KEY = 'sb_publishable_5KwhRFvxKJBZrmV4qWd_Fw_13IMo8-O';
+
+export const MAP_STYLE = 'https://tiles.openfreemap.org/styles/positron';
