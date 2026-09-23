3d347bdbcdfe982c146f4d2c2a638560dd4f6837
Totomato <thomas.cosnier@etu.univ-paris1.fr>
2026-09-23 15:23:37 +0200

    Delete js directory

--- js/config.js
+++
@@ -1,6 +0,0 @@
-
-export const SUPABASE_URL = 'https://mzkpaqqrylhzabyckimh.supabase.co';
-
-export const SUPABASE_KEY = 'sb_publishable_5KwhRFvxKJBZrmV4qWd_Fw_13IMo8-O';
-
-export const MAP_STYLE = 'https://tiles.openfreemap.org/styles/positron';
